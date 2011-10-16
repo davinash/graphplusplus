b02188653bade6282b9886ad3a237de467593c28
dongre avinash <dongre.avinash@gmail.com>
2011-10-17 03:04:05 +0530

    basic changes done

--- Graph/src/Graph.h
+++ Graph/src/Graph.h
@@ -21,8 +21,8 @@
 *  THE SOFTWARE.
 ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **/
 
-#ifndef _GRAPH_H__
-#define _GRAPH_H__
+#ifndef GRAPH_H__
+#define GRAPH_H__
 
 #include <map>
 #include <list>
@@ -41,432 +41,164 @@
 // decorated name length exceeded, name was truncated
 #pragma warning(disable:4503)
 
+
+enum graph_type {
+  DIRECTED,
+  UNDIRECTED
+};
+
 enum vertex_color  {
-	BLACK,
+  BLACK,
 	WHITE,
 	GREY
 };
 
-enum graph_type {
-	DIRECTED,
-	UNDIRECTED
-};
 
-enum graph_operation {
-	TOPOLOGICALSORT
-};
-
-template <typename U>
-class union_find {
-private:
-	struct element {
-		U m_value;
-		int m_parent;
-		int m_rank;
-
-		element(const U& value, int parent)
-			:   m_value(value), m_parent(parent), m_rank(0)
-		{}
-	};
-private:
-	std::map<int,element> m_Elements;
-public:
-	union_find(){}
-public:
-	void AddElement(int x, const U& value = U()) {
-		m_Elements.insert(std::make_pair(x, element(value, x)));
-	}
-	int FindSet(int x) {
-		element& element = GetElement(x);
-		int& parent = element.m_parent;
-		if(parent != x) {
-			parent = FindSet(parent);
-		}
-		return parent;
-	}
-	void UnionSets(int x, int y) {
-		int left  = FindSet(x);
-		int right = FindSet(y);
-		if(left != right) SetupParentRelationship(left, right);
-	}
-private:
-	element& GetElement(int x) {
-		typename std::map<int,element>::iterator it = m_Elements.find(x);
-		return it->second;
-	}
-
-	void SetupParentRelationship(int x, int y) {
-		element& elementX = GetElement(x);
-		element& elementY = GetElement(y);
-		int& rankX = elementX.m_rank;
-		int& rankY = elementY.m_rank;
-		if(rankX > rankY) {
-			elementY.m_parent = x;
-		} else {
-			elementX.m_parent = y;
-			if(rankX == rankY) ++rankY;
-		}
-	}
-};
-
-template <typename V>
-class vertex {
-public:
-	vertex(V in) :	m_data(in){ }
-	~vertex() { }
-	vertex( vertex const &right) : m_data(right.m_data){}
-
-	bool operator < ( const vertex & right) const {
-		return m_data < right.m_data;
-	}
-
-	bool operator == ( const vertex & right ) const {
-		return m_data == right.m_data;
-	}
-
-	friend std::ostream& operator << (std::ostream& os, 
-		const vertex& vertex) {
-			return os << vertex.m_data;	
-	}
-	V get_remote_data() { return m_data;}
-private:
-	V m_data;
-protected:
-};
-
-template <typename I>
-struct  comp_iterator {
-	bool operator()(I left,I right) { 
-		return *left < *right; 
-	}
-};
+template <typename T>
+struct  comp_vertex {
+  bool operator()(T left,T right) { 
+    return *left < *right; 
+    }
+  };
 
 template <typename P>
-struct  comp_pair {
+struct  comp_edge {
 	bool operator()(P left,P right) { 
 		return (*(left.first) < *(right.first) && 
-			*(left.second) < *(right.second)); 
+		        *(left.second) < *(right.second)); 
 	}
 };
-
-template <typename G>
+// graph template class.
+template <typename T, typename W = int >
 class graph {
-private:		
 public:
-	typedef typename vertex<G>                              vertex_t;
-	typedef typename std::set<vertex_t>                     graph_storage_t;
-	typedef typename std::list<vertex_t *>                  adj_list_t;    
-	typedef typename std::map<vertex_t*, adj_list_t, 
-		                      comp_iterator<vertex_t*> >    graph_t;    
-	typedef typename std::map<vertex_t*, int, 
-		                      comp_iterator<vertex_t*> >    distance_map_t;
-	typedef typename distance_map_t TimeMap_t;
-	typedef typename std::map<vertex_t*, vertex_t*, 
-		                      comp_iterator<vertex_t*> >    parent_map_t;
-	typedef typename std::pair<vertex_t*, vertex_t*>        edge_t ;
-	typedef typename std::vector<std::pair<int, edge_t> >   edge_vector_t;	
-	typedef typename std::map<edge_t, int,
-		                      comp_pair<edge_t> >           edge_weight_t;
-	typedef typename std::map<vertex_t*, vertex_color>      color_map_t;
-	typedef typename graph_storage_t::iterator              graph_storage_itr;
-	typedef typename adj_list_t::iterator                   adj_list_itr;
-	typedef typename graph_t::iterator                      graph_itr;
-	typedef typename edge_vector_t::iterator                edge_vector_iter;
+  // Some typedef for convience.
+  typedef typename std::list<T*>                             adj_list_t;
+  typedef typename adj_list_t::iterator                      adj_list_itr_t;
 
+  typedef typename comp_vertex<T*>                           graph_comp_t;
+  typedef typename std::map<T*, adj_list_t, graph_comp_t >   graph_t;
+  typedef typename graph_t::iterator                         graph_itr_t;
+
+  typedef typename std::set<T>                               storge_t;
+  typedef typename storge_t::iterator                        storage_itr_t;
+
+  typedef typename std::pair<T*, T*>                         edge_t ;
+  typedef typename comp_edge<edge_t>                         comp_edge_t;
+	typedef typename std::map<edge_t, W, comp_edge_t >         edge_weight_map_t;
+
+  typedef typename std::map<T*, vertex_color>                color_map_t;
+
+  typedef typename std::map<T*, W,  graph_comp_t >           distance_map_t;
+	typedef typename std::map<T*, T*, graph_comp_t >           parent_map_t;
 public:
-	graph(graph_type eType = UNDIRECTED) : m_Type(eType) {
-		m_TopologicalSortList = std::list<vertex_t *>();
-	}
+  // Construct object of graph type.
+  // Accept type of the graph
+  // Available type are
+  // 1. Undirected graph
+  // 2. Directed graph
+  // Default graph type is Undirected.
+  graph(graph_type eType = UNDIRECTED) : m_type(eType) {
+    m_intermediate_values.clear();
+    }
+  // destructor
+  // Does not call destructor for each of the elements in the container
+  // It is important the user of this calls destructor for each of the 
+  // element explicitly.
+  ~graph() {
+    m_intermediate_values.clear();
+    m_storage.clear();
+  }
+  // insert vertex in the graph
+  // if the vertex is already in the graph
+  // function just return and does nothing.
+  // also initialize the Adjacency list of the vertex.
+  void insert ( T vertex ) {
+    if ( find_vertex_address(vertex) == m_storage.end()) {
+      // insert the vertex data in the graph storage
+      // this the only place where actual vertex is stored
+      // all other places, address of this will be used.
+      m_storage.insert(vertex);
+      // insert the vertex address from storage into the 
+      // graph and intiailize empty adjacency list.
+      m_graph[&*(find_vertex_address(vertex))] = adj_list_t();
+    }
+  }
+  // insert
+  // inserts an edge in the graph
+  void insert(T left , T right) {
+    insert(left);
+    insert(right);
 
-	~graph() {}
+    T *vL  = &*(find_vertex_address(left));
+    T *vR  = &*(find_vertex_address(right));
 
-	void add_vertex(vertex_t vertexID) {
-		graph_storage_itr pos = m_GraphStorage.find(vertexID);
-		if ( pos == m_GraphStorage.end()) {
-			m_GraphStorage.insert(vertexID);
-			m_Graph[&(*(m_GraphStorage.find(vertexID)))] = adj_list_t();
-		}	
-	}
-
-	void add_vertex(G vVertex) {
-		this->add_vertex((vertex_t) vVertex);
-	}
-
-	adj_list_t GetAdjList(vertex_t vertexID) {
-		vertex_t *vVertexIDPtr  = &(*(m_GraphStorage.find(vertexID)));
-		graph_itr iter = m_Graph.find(vVertexIDPtr);
-		adj_list_t list;
-		if ( iter != m_Graph.end()){
-			return iter->second;
+    m_graph[vL].push_back(vR);
+    if ( m_type == UNDIRECTED) {
+      m_graph[vR].push_back(vL);
 		}
-		return list;
-	}
+  }
+  // insert add edge to the graph with the weight
+  void insert( T left, T right, W weight) {
+    insert(left, right );
+    T *vL  = &*(find_vertex_address(left));
+    T *vR = &*(find_vertex_address(right));
+    m_edge_weight_map.insert(std::make_pair(std::make_pair(vL,vR),weight));
+  }
+  void breadh_first_search(T* S = (T*)0, bool internal = false) {
+    m_intermediate_values.clear();
+    m_bfs_distance_map.clear();
+    m_bfs_parent_map.clear();
 
-	void add_edge(vertex_t vLeft, vertex_t vRight) {
-		add_vertex(vLeft);
-		add_vertex(vRight);	
+    color_map_t col_map;
 
-		vertex_t *vLeftPtr  = &(*(m_GraphStorage.find(vLeft)));
-		vertex_t *vRightPtr = &(*(m_GraphStorage.find(vRight)));
-
-		m_Graph[vLeftPtr].push_back(vRightPtr);
-		if ( m_Type == UNDIRECTED) {
-			m_Graph[vRightPtr].push_back(vLeftPtr);
+    if (!S ) {
+      S = m_graph.begin()->first;
+    }
+    for ( graph_itr_t itr = m_graph.begin();itr != m_graph.end(); ++itr) {
+      T *U                  = itr->first;
+      col_map[U]            = WHITE;
+      m_bfs_distance_map[U] = -1;
+      m_bfs_parent_map[U]   = (T*)0;
 		}
-	}
+    col_map[S]              = GREY;
+    m_bfs_distance_map[S]   = 0;
+    m_bfs_parent_map[S]     = (T*)0;
 
-	void add_edge(G vLeft, G vRight) {
-		this->add_edge((vertex_t) vLeft, (vertex_t) vRight);
-	}
-
-	void add_edge(G vLeft, G vRight, int weight) {
-		this->add_edge((vertex_t) vLeft, (vertex_t) vRight);
-
-		vertex_t *vLeftPtr  = &(*(m_GraphStorage.find(vLeft)));
-		vertex_t *vRightPtr = &(*(m_GraphStorage.find(vRight)));
-
-		m_EdgeVector.push_back(std::make_pair(weight, 
-			std::make_pair(vLeftPtr,
-			vRightPtr)));
-
-		m_EdgeWeight.insert(std::make_pair(
-			std::make_pair(vLeftPtr,vRightPtr),
-			weight));
-	}
-
-	void BFS(vertex_t *pStart = (vertex_t*)0) {
-		if ( ! pStart) {
-			pStart = m_Graph.begin()->first;
-		}
-		_BFS(pStart);
-
-	}
-
-	void DFS(vertex_t *pStart = (vertex_t*)0) {
-		if ( ! pStart) {
-			pStart = m_Graph.begin()->first;
-		}
-		_DFS(pStart,0);
-	}
-
-
-	bool GetShortestPath(G S, G V, std::vector<G> &outPath) {
-		vertex_t *pS  = (vertex_t*)0;
-		vertex_t *pV =  (vertex_t*)0;
-		if ( m_GraphStorage.find(S) != m_GraphStorage.end()){
-			pS  = &(*(m_GraphStorage.find(S)));
-		}
-		if ( m_GraphStorage.find(V) != m_GraphStorage.end()){
-			pV  = &(*(m_GraphStorage.find(V)));
-		}
-		if ( !pS || !pV){
-			return false;
-		}
-		_BFS(pS);
-		_GetShortestPath(pS, pV, outPath);
-		return true;
-	}
-
-	bool SortToplogical( std::list<G> &outList) {
-		m_Operation = TOPOLOGICALSORT;
-		_DFS(m_Graph.begin()->first);
-		for ( std::list<vertex_t *>::iterator it = m_TopologicalSortList.begin();
-			it != m_TopologicalSortList.end(); ++it) {
-				outList.push_back((*it)->get_remote_data());
-		}
-		m_TopologicalSortList.clear();
-		return true;
-	}
-
-	void BuildMST() {
-		union_find<vertex_t*> ufSet;
-		int index = 0;
-		std::map<vertex_t*, int> VertexArray;
-		//MAKE-SET(v)
-		for ( graph_itr it = m_Graph.begin();it != m_Graph.end(); ++it) {
-			vertex_t *U = it->first;
-			ufSet.AddElement(index, U);
-			VertexArray.insert(std::make_pair(U,index));
-			index++;
-		}
-		//sort the edges of E into nondecreasing order by weight w
-		std::sort(m_EdgeVector.begin(), m_EdgeVector.end());
-		for ( edge_vector_iter it = m_EdgeVector.begin(); 
-			                                  it != m_EdgeVector.end(); ++it) {
-			vertex_t *pLeft  = (it->second).first;
-			vertex_t *pRight = (it->second).second;
-			int leftIndex    = (VertexArray.find(pLeft))->second;
-			int rightIndex   = (VertexArray.find(pRight))->second;
-			if ( ufSet.FindSet(leftIndex) != ufSet.FindSet(rightIndex)){
-				ufSet.UnionSets(leftIndex,rightIndex);
-			}
-		}
-	}    
-private:
-	void _relax(vertex_t* u, vertex_t* v, int w, 
-		        distance_map_t &d, parent_map_t &p){
-
-		int dv = d.find(v)->second;
-		int du = d.find(u)->second;
-
-		if(dv > (du + w)) {
-			d[v] = du + w;
-			p[v] = u;
-		}
-	}
-
-	bool _GetShortestPath(vertex_t *S, vertex_t *V, 
-		                               std::vector<G> & pathVector){
-		if (*S == *V) {
-			pathVector.push_back(V->get_remote_data());
-		} else {
-			if ( m_BFSParentMap[V] == ( vertex_t*)0)
-				return false;
-			else
-				_GetShortestPath(S,m_BFSParentMap[V],pathVector);
-
-			pathVector.push_back(V->get_remote_data());
-		}
-		return true;
-	}
-
-	void _BFS(vertex_t *S) {
-		color_map_t colorMap;
-		m_BFSDistanceMap.clear();
-		m_BFSParentMap.clear();
-		for ( graph_itr itr = m_Graph.begin();itr != m_Graph.end(); ++itr) {
-			vertex_t *U = itr->first;
-			//U->m_Color =  WHITE;
-			colorMap[U] = WHITE;
-			m_BFSDistanceMap[U] = -1;
-			m_BFSParentMap[U] = (vertex_t *)0;
-		}
-		//S->m_Color = GREY;
-		colorMap[S] = GREY;
-		// Initialize the d to 0
-		m_BFSDistanceMap[S] = 0;
-		m_BFSParentMap[S] = (vertex_t *)0;
-
-		std::deque<vertex_t*> Q;
-		// Enque s ( given vertex ) to Queue Q
+		std::deque<T*> Q;
 		Q.push_back(S);
 		while ( !Q.empty()) {
-			vertex_t *U  = Q.front();
+			T* U  = Q.front();
 			Q.pop_front();
-			adj_list_t vAdjList = m_Graph.find(U)->second;
-
-			for ( adj_list_itr itr = vAdjList.begin(); 
-				                            itr != vAdjList.end(); ++itr) {
-				vertex_t *V = *(itr);
-				if ( colorMap.find(V)->second == WHITE ) {
-					colorMap[V] = GREY;
-					m_BFSDistanceMap[V] = m_BFSDistanceMap[U] + 1;
-					m_BFSParentMap[V] = U;
+			adj_list_itr_t adj_list = m_graph.find(U)->second;
+			/*for ( adj_list_itr_t it = adj_list.begin(); it != adj_list.end(); ++it) {
+				T *V = *(it);
+				if ( col_map.find(V)->second == WHITE ) {
+					col_map[V]             = GREY;
+					m_bfs_distance_map[V]  = m_bfs_distance_map[U] + 1;
+					m_bfs_parent_map[V]    = U;
 					Q.push_back(V);
 				}
-			}
-			colorMap[U] = BLACK;
-		} // Queue empty loop
-	}
-
-	void _DFS(vertex_t *S) {
-		color_map_t colorMap;
-		m_DFSStartTimeMap.clear();
-		m_DFSFinishTimeMap.clear();
-		m_DFSParentMap.clear();		
-		for ( graph_itr itr = m_Graph.begin();itr != m_Graph.end(); ++itr) {
-			colorMap[itr->first] = WHITE;
-			m_DFSParentMap[itr->first] = (vertex_t*)0;
+			}*/
+			col_map[U] = BLACK;
 		}
-		int time = 0;
-		for ( graph_itr itr = m_Graph.begin();itr != m_Graph.end(); ++itr) {
-			if ( colorMap.find(itr->first)->second == WHITE ) {
-				_DFS_Visit(	itr->first,
-					m_DFSStartTimeMap,
-					m_DFSFinishTimeMap,
-					m_DFSParentMap, 
-					time,
-					colorMap);
-			}
-		}
-	}
-
-	void _DFS_Visit(vertex_t *inU, TimeMap_t & d, 
-		TimeMap_t & f, parent_map_t & p , 
-		int & time, color_map_t &colorMap) {
-			std::stack<vertex_t *> S;
-			S.push(inU);
-			while ( !S.empty()) {
-				vertex_t *U  = S.top();				
-				adj_list_t vAdjList = m_Graph.find(U)->second;
-				adj_list_itr itr;
-				if ( colorMap.find(U)->second == WHITE ) {  
-					time = time + 1;
-					d[U] = time;
-					colorMap[U] = GREY;
-					for ( itr = vAdjList.begin(); itr != vAdjList.end(); ++itr){
-						vertex_t *V = *(itr);
-						if ( colorMap.find(V)->second == WHITE ) {
-							p[V] = U;
-							S.push(V);
-							break;
-						}
-					}
-				} else {
-					itr = vAdjList.end();
-				}
-				if ( itr == vAdjList.end() && 
-					               colorMap.find(U)->second == GREY ) {
-					time = time + 1;
-					colorMap[U] = BLACK;
-					f[U] = time;
-					// For topological sorting
-					if (m_Operation == TOPOLOGICALSORT) {
-						m_TopologicalSortList.push_front(U);
-					}
-					S.pop();
-				}
-			}
-	}
-
-public:
-	friend std::ostream& operator << (std::ostream& os, graph& graph) {
-		std::cout << "-------------------------------------------" << std::endl;
-		for ( graph_storage_itr	iter = graph.m_GraphStorage.begin(); 
-			iter != graph.m_GraphStorage.end(); 
-			++iter) {
-				std::cout << *iter << " : " ;
-				adj_list_t list = graph.GetAdjList(*iter);
-				adj_list_itr iter1;
-				for ( iter1 = list.begin(); iter1 != list.end(); ++iter1) {
-					std::cout << *(*iter1) << '\t' ;
-				}
-				std::cout << std::endl;
-		}
-		std::cout << "-------------------------------------------" << std::endl;
-		return os;
-	}
+  }
 private:
-	graph_type       m_Type; 
-	graph_t          m_Graph;
-	edge_vector_t    m_EdgeVector;
-	graph_storage_t  m_GraphStorage;
-	edge_weight_t    m_EdgeWeight;
+  storage_itr_t find_vertex_address(T vertex) {
+    return m_storage.find(vertex);
+  }
+private:
+  graph_type m_type; // 1. directed or un-directed
+  // holds the intermediates values and will be used by mainly by
+  // iterator interface.
+  std::vector<T*>    m_intermediate_values; 
+  adj_list_t         m_adj_list;
+  graph_t            m_graph;
+  storge_t           m_storage;
+  edge_weight_map_t  m_edge_weight_map;
 
-	// Distance map
-	distance_map_t   m_BFSDistanceMap;
-	// Visit Time Map
-	TimeMap_t        m_DFSStartTimeMap;
-	TimeMap_t        m_DFSFinishTimeMap;
-	// Parent Map
-	parent_map_t     m_BFSParentMap;
-	parent_map_t     m_DFSParentMap;
-	// topological sorting list.
-	std::list<vertex_t *> m_TopologicalSortList;
-	graph_operation m_Operation;
-
-protected:
+  distance_map_t     m_bfs_distance_map;
+  parent_map_t       m_bfs_parent_map;
 };
-
 
 #endif
--- Graph/src/TGraph.cpp
+++ Graph/src/TGraph.cpp
@@ -25,115 +25,153 @@
 #include <string>
 #include <assert.h>
 
-void BuildMyGraphShortPathTest(graph<std::string> &G) {	
-	G.add_edge("v","r");
-	G.add_edge("r","s");
-	G.add_edge("s","w");
-	G.add_edge("w","x");
-	G.add_edge("w","t");
-	G.add_edge("t","x");
-	G.add_edge("t","u");
-	G.add_edge("x","u");
-	G.add_edge("x","y");
-	G.add_edge("u","y");
-}
 
-void BuildMyGraphTS(graph<std::string> &G) {
-
-	G.add_edge("A","D");
-	G.add_edge("A","E");
-
-	G.add_edge("B","E");
-
-	G.add_edge("C","F");
-	G.add_edge("C","G");
-	G.add_edge("C","A");
-	G.add_edge("C","B");
-
-	G.add_edge("D","E");
-	G.add_edge("D","F");
-
-	G.add_edge("E","F");
-
-	G.add_edge("F","H");
-	
-}
-void BuildMyWeightedGraph(graph<std::string> &G) {
-	G.add_edge("A", "B", 1);
-	G.add_edge("A", "D", 2);	
-	G.add_edge("A", "F", 2);
-
-	G.add_edge("B", "D", 2);	
-	G.add_edge("B", "C", 1);
-
-	G.add_edge("C", "D", 1);
-	G.add_edge("C", "E", 3);	
-
-	G.add_vertex ("D");
-
-	G.add_edge("E", "D", 2);
-	G.add_edge("E", "G", 1);	
-
-	G.add_edge("F", "D", 1);	
-
-	G.add_edge("G", "D", 3);
-	G.add_edge("G", "F", 3);	
-
-}
-
-void BuildBFGraph(graph<std::string> &G){
-	G.add_edge("s","t",6);
-	G.add_edge("s","y",7);
-	G.add_edge("t","x",5);
-	G.add_edge("t","y",8);
-	G.add_edge("x","t",-2);
-	G.add_edge("z","x",7);
-	G.add_edge("z","s",2);
-	G.add_edge("y","z",9);
-	G.add_edge("y","x",-3);
-	G.add_edge("t","z",-4);
-}
 int main(int argc, char **argv) {
-
-	std::string expctedResult = "";
-	std::string actualResult  = "";
-
-	// Test 1
-	graph<std::string> myGraphShortestPath;
-	BuildMyGraphShortPathTest(myGraphShortestPath);
-	std::vector<std::string> path;
-	myGraphShortestPath.GetShortestPath("r","y",path);
-	expctedResult = "rswxy";
-	actualResult  = "";
-	for ( std::vector<std::string>::iterator itr  = path.begin();
-		itr != path.end(); ++itr) {
-			actualResult.append(*itr);
-	}
-	assert ( expctedResult == actualResult);
-	expctedResult = "";
-	actualResult  = "";
-
-	// Test 2
-	graph<std::string> myGraphTS(DIRECTED);
-	BuildMyGraphTS(myGraphTS);
-	std::list<std::string> listTS;
-	myGraphTS.SortToplogical(listTS);
-	expctedResult = "CGBADEFH";
-	for ( std::list<std::string>::iterator itr  = listTS.begin();
-		itr != listTS.end(); ++itr) {
-			actualResult.append(*itr);
-	}
-	assert ( expctedResult == actualResult);
-	expctedResult = "";
-	actualResult  = "";
-
-	// Test 3
-	graph<std::string> myGraphWeight;
-	BuildMyWeightedGraph(myGraphWeight);
-	myGraphWeight.BuildMST();
-
-	// Test 4
-	graph<std::string> bfGraph;
-	BuildBFGraph(bfGraph);
-	graph<std::string>::distance_map_t p;
+	graph<std::string> g;
+	g.insert("A","B");
+  g.breadh_first_search();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//BuildMyGraphShortPathTest(myGraphShortestPath);
+	//std::vector<std::string> path;
+	//myGraphShortestPath.GetShortestPath("r","y",path);
+	//expctedResult = "rswxy";
+	//actualResult  = "";
+	//for ( std::vector<std::string>::iterator itr  = path.begin();
+	//	itr != path.end(); ++itr) {
+	//		actualResult.append(*itr);
+	//}
+	//assert ( expctedResult == actualResult);
+	//expctedResult = "";
+	//actualResult  = "";
+
+	//// Test 2
+	//graph<std::string> myGraphTS(DIRECTED);
+	//BuildMyGraphTS(myGraphTS);
+	//std::list<std::string> listTS;
+	//myGraphTS.SortToplogical(listTS);
+	//expctedResult = "CGBADEFH";
+	//for ( std::list<std::string>::iterator itr  = listTS.begin();
+	//	itr != listTS.end(); ++itr) {
+	//		actualResult.append(*itr);
+	//}
+	//assert ( expctedResult == actualResult);
+	//expctedResult = "";
+	//actualResult  = "";
+
+	//// Test 3
+	//graph<std::string> myGraphWeight;
+	//BuildMyWeightedGraph(myGraphWeight);
+	//myGraphWeight.BuildMST();
+
+	//// Test 4
+	//graph<std::string> bfGraph;
+	//BuildBFGraph(bfGraph);
+	//graph<std::string>::distance_map_t p;
+//}
+
+
+
+//void BuildMyGraphShortPathTest(graph<std::string> &G) {	
+//	G.insert("v","r");
+//	G.insert("r","s");
+//	G.insert("s","w");
+//	G.insert("w","x");
+//	G.insert("w","t");
+//	G.insert("t","x");
+//	G.insert("t","u");
+//	G.insert("x","u");
+//	G.insert("x","y");
+//	G.insert("u","y");
+//}
+//
+//void BuildMyGraphTS(graph<std::string> &G) {
+//
+//	G.insert("A","D");
+//	G.insert("A","E");
+//
+//	G.insert("B","E");
+//
+//	G.insert("C","F");
+//	G.insert("C","G");
+//	G.insert("C","A");
+//	G.insert("C","B");
+//
+//	G.insert("D","E");
+//	G.insert("D","F");
+//
+//	G.insert("E","F");
+//
+//	G.insert("F","H");
+//	
+//}
+//void BuildMyWeightedGraph(graph<std::string> &G) {
+//	G.insert("A", "B", 1);
+//	G.insert("A", "D", 2);	
+//	G.insert("A", "F", 2);
+//
+//	G.insert("B", "D", 2);	
+//	G.insert("B", "C", 1);
+//
+//	G.insert("C", "D", 1);
+//	G.insert("C", "E", 3);	
+//
+//	G.add_vertex ("D");
+//
+//	G.insert("E", "D", 2);
+//	G.insert("E", "G", 1);	
+//
+//	G.insert("F", "D", 1);	
+//
+//	G.insert("G", "D", 3);
+//	G.insert("G", "F", 3);	
+//
+//}
+//
+//void BuildBFGraph(graph<std::string> &G){
+//	G.insert("s","t",6);
+//	G.insert("s","y",7);
+//	G.insert("t","x",5);
+//	G.insert("t","y",8);
+//	G.insert("x","t",-2);
+//	G.insert("z","x",7);
+//	G.insert("z","s",2);
+//	G.insert("y","z",9);
+//	G.insert("y","x",-3);
+//	G.insert("t","z",-4);
+//}
